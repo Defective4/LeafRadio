fa98378644a9cdb52c912d43fdb744222af8e62d
Defective <def3ctive4@gmail.com>
2025-07-24 10:01:22 +0200

    Improved the URL dialog

--- src/main/java/io/github/defective4/springfm/client/components/ServerConnectDialog.java
+++ src/main/java/io/github/defective4/springfm/client/components/ServerConnectDialog.java
@@ -1,5 +1,6 @@
 package io.github.defective4.springfm.client.components;
 
+import java.net.URI;
 import java.net.URL;
 
 import org.eclipse.swt.SWT;
@@ -9,8 +10,10 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Dialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
@@ -42,7 +45,7 @@ public class ServerConnectDialog extends Dialog {
     private void createContents() {
         shell = new Shell(getParent(), SWT.CLOSE | SWT.APPLICATION_MODAL);
         shell.setText(getText());
-        GridLayout gl_shell = new GridLayout(2, false);
+        GridLayout gl_shell = new GridLayout(1, false);
         gl_shell.marginTop = 5;
         gl_shell.marginBottom = 16;
         gl_shell.marginLeft = 16;
@@ -50,16 +53,46 @@ public class ServerConnectDialog extends Dialog {
         shell.setLayout(gl_shell);
 
         new Label(shell, SWT.NONE).setText("Enter URL of a SpringFM server to connect to:");
-        new Label(shell, SWT.NONE);
 
         Combo combo = new Combo(shell, SWT.NONE);
         combo.setItems(new String[] { "http://localhost:8080" }); // TODO
         combo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 
-        Button btnConnect = new Button(shell, SWT.NONE);
+        Button rememberCheck = new Button(shell, SWT.CHECK);
+        rememberCheck.setSelection(true);
+        rememberCheck.setText("Remember URL");
+
+        Composite buttons = new Composite(shell, SWT.NONE);
+        buttons.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+        buttons.setLayout(new RowLayout(SWT.HORIZONTAL));
+
+        Button btnCancel = new Button(buttons, SWT.NONE);
+        btnCancel.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                shell.dispose();
+            }
+        });
+        btnCancel.setText("Cancel");
+
+        Button btnConnect = new Button(buttons, SWT.NONE);
         btnConnect.setEnabled(false);
         btnConnect.setText("Connect");
-
+        btnConnect.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent ev) {
+                try {
+                    URL url = URI.create(combo.getText()).toURL();
+                    if (!url.getProtocol().startsWith("http") || url.getHost().isBlank())
+                        throw new IllegalArgumentException();
+                    result = url;
+                    shell.dispose();
+                } catch (Exception e) {
+                    btnConnect.setEnabled(false);
+                }
+            }
+        });
+        shell.setDefaultButton(btnConnect);
         shell.pack();
 
         combo.addModifyListener(new ModifyListener() {
@@ -70,20 +103,6 @@ public class ServerConnectDialog extends Dialog {
                     if (!url.getProtocol().startsWith("http") || url.getHost().isBlank())
                         throw new IllegalArgumentException();
                     btnConnect.setEnabled(true);
-                } catch (Exception e) {
-                    btnConnect.setEnabled(false);
-                }
-            }
-        });
-        btnConnect.addSelectionListener(new SelectionAdapter() {
-            @Override
-            public void widgetSelected(SelectionEvent ev) {
-                try {
-                    URL url = new URL(combo.getText());
-                    if (!url.getProtocol().startsWith("http") || url.getHost().isBlank())
-                        throw new IllegalArgumentException();
-                    result = url;
-                    shell.dispose();
                 } catch (Exception e) {
                     btnConnect.setEnabled(false);
                 }
